3e41d831297374f2945fd0e5ffbbdb2bd8614d7e
Paul Klein <paulleoklein@gmail.com>
2020-08-10 03:03:13 -0700

    Updates

---
+++ component---src-pages-about-js-f2129bbfbdde0b4a8505.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"3XHS":function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return l}));var a=t("q1tI"),r=t.n(a);function c(){return r.a.createElement("h1",null,"This is a header.")}function l(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c,null),r.a.createElement("p",null,"Such wow. Very React."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-f2129bbfbdde0b4a8505.js.map
---
+++ component---src-pages-index-js-1b5ee0cf24221fc67990.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{RXBc:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return r}));var a=n("q1tI"),l=n.n(a);function r(){return l.a.createElement("div",{style:{color:"purple"}},l.a.createElement("h1",null,"Hello Gatsby!"),l.a.createElement("p",null,"What a world."),l.a.createElement("img",{src:"https://source.unsplash.com/random/400x200",alt:""}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-1b5ee0cf24221fc67990.js.map
